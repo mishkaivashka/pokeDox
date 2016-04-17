402f57d3e3bf953f4a8d6afadc0647482df7eddf
Mihails Ivanovs <Misha@mihailss-macbook-pro.local>
2016-04-17 14:38:24 +0300

    Initial Commit

---
+++ pokeDox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeDox
+//
+//  Created by Mihails Ivanovs on 17/04/2016.
+//  Copyright © 2016 Mihails Ivanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
